b8101278357057185b879659cbfa61ec825f9775
Aleksander Czubiak <olek@MacBook-Pro-Aleksander.local>
2021-03-22 22:53:12 +0100

    parse-InternetowyKantor_v2.0 | rm test.py

--- WebScappingServer/download/parse_internetowykantor.py
+++ WebScappingServer/download/parse_internetowykantor.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 name = ['eur', 'chf', 'usd', 'gbp']
 to_buy = "currency_table_buy bem-rate-table__rate"
 to_sell = "currency_table_sell bem-rate-table__rate"
-i = 0
+
 # Poprawić na odczytywanie pliku
 FILE = 'html/html_internetowykantor.html'
 
@@ -12,10 +12,12 @@ def open_file(filename):
     return open(filename, 'r', encoding='utf8').read()
 
 
-def search(filename, name, i):
+def add_to_db():
+    pass
+
+
+def search(filename, name, i = 0):
     soup = BeautifulSoup(open_file(filename, ), "html.parser")
-    #finder_to_buy = soup.body.find('tr', attrs={'data-currency-id': name}).find('td', attrs={'class': to_buy}).text
-    #finder_to_buy = finder_to_buy.find('td', attrs={'class': to_buy}).text
 
     def finder_to_buy(name):
         to_buy_list = []
@@ -35,19 +37,9 @@ def search(filename, name, i):
     return {name[i].upper(): {'BUY': buy_list[i], "SELL": sell_list[i]}}
 
 
-
-
-def add_to_db():
-    pass
-
-
 def print_all_currency():
     for j in range(4):
         print(search(FILE, name, j))
-
-
-
-
 
 if __name__ == "__main__":
     print_all_currency()
