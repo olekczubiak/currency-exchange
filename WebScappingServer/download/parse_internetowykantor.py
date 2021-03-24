937da8835c758d122ccf94b3128dead556ea4e1e
Jan Koprowski <jkoprowski14@gmail.com>
2021-03-24 19:54:51 +0100

    Usuwanie-zbednego-kodu

--- WebScappingServer/download/parse_internetowykantor.py
+++ WebScappingServer/download/parse_internetowykantor.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 name = ['eur', 'chf', 'usd', 'gbp']
 to_buy = "currency_table_buy bem-rate-table__rate"
 to_sell = "currency_table_sell bem-rate-table__rate"
-i = 0
+
 # Poprawić na odczytywanie pliku
 FILE = 'html/html_internetowykantor.html'
 
@@ -12,21 +12,19 @@ def open_file(filename):
     return open(filename, 'r', encoding='utf8').read()
 
 
-def search(filename, name, i):
+def search(filename, name, i=0):
     soup = BeautifulSoup(open_file(filename, ), "html.parser")
-    #finder_to_buy = soup.body.find('tr', attrs={'data-currency-id': name}).find('td', attrs={'class': to_buy}).text
-    #finder_to_buy = finder_to_buy.find('td', attrs={'class': to_buy}).text
 
     def finder_to_buy(name):
         to_buy_list = []
-        for i in name:
-            to_buy_list.append(soup.body.find('tr', attrs={'data-currency-id': i}).find('td', attrs={'class': to_buy}).text)
+        for j in name:
+            to_buy_list.append(soup.body.find('tr', attrs={'data-currency-id': j}).find('td', attrs={'class': to_buy}).text)
         return to_buy_list
 
     def finder_to_sell(name):
         to_sell_list = []
-        for i in name:
-            to_sell_list.append(soup.body.find('tr', attrs={'data-currency-id': i}).find('td', attrs={'class': to_sell}).text)
+        for j in name:
+            to_sell_list.append(soup.body.find('tr', attrs={'data-currency-id': j}).find('td', attrs={'class': to_sell}).text)
         return to_sell_list
 
     buy_list = finder_to_buy(name)
