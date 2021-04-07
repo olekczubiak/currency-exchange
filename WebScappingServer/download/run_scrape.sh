cfacf19b87b76066f7f1b5d5795e5ff0d6ff2065
Aleksander Czubiak <olek@MacBook-Pro-Aleksander.local>
2021-04-07 21:50:53 +0200

    make main class

--- WebScappingServer/download/run_scrape.sh
+++ WebScappingServer/download/run_scrape.sh
@@ -2,18 +2,18 @@
 # chmod +x this file to run!!!
 
 echo -ne '#                          (4%)\r'
-rm html/* ### DEL OLD FILES
-sleep 1 
+# rm html/* ### DEL OLD FILES
+# sleep 1 
 echo -ne '##                         (12%)\r'
 echo -ne '######                     (25%)\r'
-python get_website.py ### DOWNLOAD 
+# python get_website.py ### DOWNLOAD 
 echo -ne '#########                  (36%)\r'
 echo -ne '############               (50%)\r'
-sleep 0.3
+# sleep 0.3
 echo -ne '################           (68%)\r'
-sleep 0.2
+# sleep 0.2
 echo -ne '##################         (75%)\r'
-sleep 0.3
+# sleep 0.3
 echo -ne '########################   (100%)\r'
 echo -ne '\n'
 
@@ -24,5 +24,5 @@ echo "Liderwalut:"
 python parse_liderwalut.py
 echo "InternetowyKantor:"
 python parse_internetowykantor.py
-read var
-echo $var
+# read var
+# echo $var
