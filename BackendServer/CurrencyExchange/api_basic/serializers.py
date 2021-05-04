7b4d08b24f64d8666f0e6e4799518fca557a1662
Jan Koprowski <jkoprowski14@gmail.com>
2021-05-04 20:26:59 +0200

    Upgraded Model

--- BackendServer/CurrencyExchange/api_basic/serializers.py
+++ BackendServer/CurrencyExchange/api_basic/serializers.py
@@ -5,9 +5,6 @@ from .models import Cantor
 class CantorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cantor
-        fields = ['name', 'date', 'website',
-                    'rating', 'buy_EUR', 'sell_EUR',
-                            'buy_GBP', 'sell_GBP',
-                            'buy_CHF', 'sell_CHF',
-                            'buy_USD', 'sell_USD']
+        fields = ['id', 'name', 'date', 'buy', 'sell', 'currency', 'website', 'rating',]
+
 
